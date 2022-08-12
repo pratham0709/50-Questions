e1ee2662c00e2448ba8c3b2a4b96030dd9d30955
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-12 18:39:08 +0530

    Create Reverse Digit.cpp

---
+++ Reverse Digit.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int reverse_digit(long long int n)
+		{
+		    // Code here
+		    long long int ans = 0;
+		    while(n!=0){
+		        long long int digit = n%10;
+		        ans = ans*10 + digit;
+		        n = n / 10;
+		    }
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.reverse_digit(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
