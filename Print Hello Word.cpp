a4688de69a49d071b43f00ed41db34ccf46206f6
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-12 18:20:41 +0530

    Create Print Hello Word.cpp

---
+++ Print Hello Word.cpp
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        //Type below cout<<"Hello World";
+        //cout will output to console
+        cout<<"Hello World";
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    Solution ob;
+    //function call
+    ob.helloWorld();
+    cout<<endl;
+    return 0;
+}
+// } Driver Code Ends
