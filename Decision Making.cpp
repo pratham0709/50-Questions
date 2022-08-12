2d7069316841d821dc49e4e510c1a83a7daf0ff6
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-12 18:22:52 +0530

    Create Decision Making.cpp

---
+++ Decision Making.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string compareFive(int n){
+        // code here 
+        if(n==5){
+            return "Equal to 5";
+        }
+        else if(n>5){
+            return "Greater than 5";
+        }
+        else{
+            return "Less than 5";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.compareFive(N) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
