303e93afdf5b33526e0e3778877b65cb585a17c9
pratham0709 <109234076+pratham0709@users.noreply.github.com>
2022-08-13 12:24:41 +0530

    Create M = Check Perfect square.cpp

---
+++ M = Check Perfect square.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int checkPerfectSquare(int N){
+        // code here
+        if(N>=0){
+             int sr = sqrt(N);
+             if(sr *sr == N)
+                return 1;
+            else{
+                return 0;
+            }
+        }
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.checkPerfectSquare(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
